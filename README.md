533f3f5980ff8f213a148fdcdde0014ecd3da250
Belousov V. Vladimir <vladimir@webatix.com>
2019-08-08 09:44:21 +0300

    hyphwords.sh added

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # fancy_scripts
-Fancy scripts for data conversion and more
+Fancy scripts for text processing and more
 
-The set of scripts, which reveals the power of regular expression for text processing:
+The set of scripts, which reveals the power of regular expressions:
 
 - html2csv.bat - batch file, which converts html tables from html file to csv file (Windows, cmd.exe, sed.exe);
 
 - html2csv.sh - shell script, which converts html tables from html file to csv file (Linux, Mac OS X, Unix, Android, bash, sed);
+
+- hyphwords.sh - shell script, which produces monospace font hyphenation by words;
---
+++ hyphwords.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cat <<7e1e656abd37b5a9c943511659456c4e
+
+ Copyright 2013 Vladimir Belousov (vlad.belos@gmail.com)
+ https://github.com/VladimirBelousov/fancy_scripts
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ Produces monospace font hyphenation by words
+
+7e1e656abd37b5a9c943511659456c4e
+
+[ -t 0 -o $# -ne 1 ]&&echo -e "Usage:\n\n cat [text file to process] | hyphwords.sh [number of characters in a line]"&&exit 1
+one=`cat`
+while [ -z "$two" -o `echo -n "$two" | sed -n '$='`0 -gt  `echo -n "$one" | sed -n '$='`0 ]; do
+  [ -n "$two" ]&&one=`echo -n "$two"`
+  two=`echo -n "$one" | sed -r ' /^.{'"$1"'}[^\\]/s/^.{'"$1"'}/&\\\\\\n/' | sed -r ' /^.{'"$1"'}[\\]/{s/([ [:punct:]])([^ ]*\\\\$)/\\1\\n\\2/;ta;s/\\\\//;:a;}' | sed -r ' /[\\]$/{N;s/\\\\\\n//}'`
+done
+echo -n "$two"
